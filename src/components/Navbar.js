88a045a000a06d03ada906b00b60573c8479795a
salmakhatun12345 <ummesalma0056@gmail.com>
2022-10-21 12:45:17 +0600

    recheck

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,29 +4,29 @@ import Modal from './SignUpModal';
 const Navbar = () => {
     return (
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-white ">
-            <div class="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-white ">
+            <div className="container">
                 <img src="https://i.ibb.co/mHRybjy/whole.png" alt="" />
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div class="navbar-nav mx-auto">
-                        <div class="navbar-nav mx-auto">
-                            <div class="row height d-flex justify-content-center align-items-center">
-                                <div class="col-md-6">
-                                    <div class="form">
-                                        <i class="fa fa-search"></i>
-                                        <input type="text" class="form-control form-input" placeholder="Search for your favorite groups in ATG" />
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="navbar-nav mx-auto">
+                        <div className="navbar-nav mx-auto">
+                            <div className="row height d-flex justify-content-center align-items-center">
+                                <div className="col-md-6">
+                                    <div className="form">
+                                        <i className="fa fa-search"></i>
+                                        <input type="text" className="form-control form-input" placeholder="Search for your favorite groups in ATG" />
                                     </div>
                                 </div>
 
                             </div>
                         </div>
                     </div>
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" aria-expanded="false">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item dropdown">
+                            <a data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat" className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" aria-expanded="false">
                                 Create account. <span className='text-primary fw-bold'>It’s free!</span>
                             </a>
                         </li>
--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -4,37 +4,37 @@ const SignUpModal = () => {
     const [open, setOpen] = useState(true)
 
     return (
-        <div class="modal mx-auto  fade mt-4" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal mx-auto  fade mt-4" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 
             {
                 !open &&
-                <div class="modal-dialog">
-                    <div style={{ width: '736px' }} class="modal-content ">
-                        <div style={{ background: '#EFFFF4', height: '50px' }} class="modal-header  ">
-                            <small style={{ fontSize: '14px', color: '#008A45' }} class="modal-title text-center" id="exampleModalLabel">Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</small>
-                            <button type="button " class="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>
+                <div className="modal-dialog">
+                    <div style={{ width: '736px' }} className="modal-content ">
+                        <div style={{ background: '#EFFFF4', height: '50px' }} className="modal-header  ">
+                            <small style={{ fontSize: '14px', color: '#008A45' }} className="modal-title text-center" id="exampleModalLabel">Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</small>
+                            <button type="button " className="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className='container'>
-                            <div class="modal-body d-flex justify-content-between gap-2 ">
+                            <div className="modal-body d-flex justify-content-between gap-2 ">
 
                                 <form>
                                     <h4>Crate  Account</h4>
 
-                                    <div class="mb-1">
-                                        <input placeholder='Email' type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <div className="mb-1">
+                                        <input placeholder='Email' type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
 
                                     </div>
-                                    <div class="mb-1">
-                                        <input placeholder='Password' type="password" class="form-control" id="exampleInputPassword1" />
+                                    <div className="mb-1">
+                                        <input placeholder='Password' type="password" className="form-control" id="exampleInputPassword1" />
                                     </div>
 
 
-                                    <button type="submit" class="btn btn-primary w-100 mt-3 rounded-pill">Sign In</button>
-                                    <button type="submit" class="btn btn-outline-secondary w-100 mt-3 "><img src="https://i.ibb.co/sqMK4Lr/f-logo-RGB-Blue-1024.png" className='ms-2' alt="" /> Sign in with Facebook</button>
-                                    <button type="submit" class="btn btn-outline-secondary w-100 mt-3 ">Sign in with Google</button>
+                                    <button type="submit" className="btn btn-primary w-100 mt-3 rounded-pill">Sign In</button>
+                                    <button type="submit" className="btn btn-outline-secondary w-100 mt-3 "><img src="https://i.ibb.co/sqMK4Lr/f-logo-RGB-Blue-1024.png" className='ms-2' alt="" /> Sign in with Facebook</button>
+                                    <button type="submit" className="btn btn-outline-secondary w-100 mt-3 ">Sign in with Google</button>
                                 </form>
 
-                                <div class="mb-3 ms-2">
+                                <div className="mb-3 ms-2">
                                     <p style={{ fontSize: '13px' }} className='text-end'>Don’t have an account yet?  <span onClick={() => setOpen(!open)} style={{ cursor: 'pointer' }} className=' text-primary fw-bold' href="">Create new for free!</span></p>
                                     <img style={{ width: '320px' }} src="https://i.ibb.co/Jx25rYd/Group-3.png" alt="" />
                                     <p style={{ fontSize: '11px' }} className=' '>By signing up,you agree to our Terms & conditions, Privacy policy</p>
@@ -48,39 +48,39 @@ const SignUpModal = () => {
 
             {
                 open &&
-                <div class="modal-dialog">
+                <div className="modal-dialog">
                     <div style={{ width: '736px' }}>
-                        <div class="modal-content ">
-                            <div style={{ background: '#EFFFF4', height: '50px' }} class="modal-header  ">
-                                <small style={{ fontSize: '14px', color: '#008A45' }} class="modal-title text-center" id="exampleModalLabel">Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</small>
-                                <button type="button " class="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>
+                        <div className="modal-content ">
+                            <div style={{ background: '#EFFFF4', height: '50px' }} className="modal-header  ">
+                                <small style={{ fontSize: '14px', color: '#008A45' }} className="modal-title text-center" id="exampleModalLabel">Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</small>
+                                <button type="button " className="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className='container'>
-                                <div class="modal-body d-flex justify-content-between gap-2 ">
+                                <div className="modal-body d-flex justify-content-between gap-2 ">
 
                                     <form>
                                         <h4>Crate  Account</h4>
-                                        <div class="input-group mb-1">
-                                            <input placeholder='First Name' type="text" aria-label="First name" class="form-control  " />
-                                            <input placeholder='Last Name' type="text" aria-label="Last name" class="form-control  " />
+                                        <div className="input-group mb-1">
+                                            <input placeholder='First Name' type="text" aria-label="First name" className="form-control  " />
+                                            <input placeholder='Last Name' type="text" aria-label="Last name" className="form-control  " />
                                         </div>
-                                        <div class="mb-1">
-                                            <input placeholder='Email' type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                        <div className="mb-1">
+                                            <input placeholder='Email' type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
 
                                         </div>
-                                        <div class="mb-1">
-                                            <input placeholder='Password' type="password" class="form-control" id="exampleInputPassword1" />
+                                        <div className="mb-1">
+                                            <input placeholder='Password' type="password" className="form-control" id="exampleInputPassword1" />
                                         </div>
-                                        <div class="mb-1">
-                                            <input placeholder='Confirm Password' type="password" class="form-control" id="exampleInputPassword1" />
+                                        <div className="mb-1">
+                                            <input placeholder='Confirm Password' type="password" className="form-control" id="exampleInputPassword1" />
                                         </div>
 
-                                        <button type="submit" class="btn btn-primary w-100 mt-3 rounded-pill">Create Account</button>
-                                        <button type="submit" class="btn btn-outline-secondary w-100 mt-3 "><img src="https://i.ibb.co/sqMK4Lr/f-logo-RGB-Blue-1024.png" className='ms-2' alt="" /> Sign up with Facebook</button>
-                                        <button type="submit" class="btn btn-outline-secondary w-100 mt-3 ">Sign up with Google</button>
+                                        <button type="submit" className="btn btn-primary w-100 mt-3 rounded-pill">Create Account</button>
+                                        <button type="submit" className="btn btn-outline-secondary w-100 mt-3 "><img src="https://i.ibb.co/sqMK4Lr/f-logo-RGB-Blue-1024.png" className='ms-2' alt="" /> Sign up with Facebook</button>
+                                        <button type="submit" className="btn btn-outline-secondary w-100 mt-3 ">Sign up with Google</button>
                                     </form>
 
-                                    <div class="mb-3 ms-2">
+                                    <div className="mb-3 ms-2">
                                         <p style={{ fontSize: '13px' }} className='text-end'>Already have an account? <span onClick={() => setOpen(!open)} style={{ cursor: 'pointer' }} className='cursor-pointer text-primary fw-bold' >Sign In</span></p>
                                         <img style={{ width: '320px', height: '320px' }} src="https://i.ibb.co/Jx25rYd/Group-3.png" alt="" />
                                         <p style={{ fontSize: '11px' }} className=' '>By signing up,you agree to our Terms & conditions, Privacy policy</p>
